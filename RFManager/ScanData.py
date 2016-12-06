d4fcbd11d835aefe6eef8b7e458ca8d968f3f9c6
mabood <mgabood8@gmail.com>
2016-12-06 04:17:10 -0800

    Fixed logging issues

--- RFManager/ScanData.py
+++ RFManager/ScanData.py
@@ -8,13 +8,13 @@ RFSCAN_FILE = get_property("RFSCAN_FILE", "CONFIGS")
 
 def main():
     if report_dir_exists():
-        dir = [f for f in listdir(RFSCAN_PATH) if isfile(join(RFSCAN_PATH, f))]
-        file = sorted(filter(lambda x: '.json' in x, dir), reverse=True)[0]
         try:
+            dir = [f for f in listdir(RFSCAN_PATH) if isfile(join(RFSCAN_PATH, f))]
+            file = sorted(filter(lambda x: '.json' in x, dir), reverse=True)[0]
             fd = open(file, 'r')
             obj = json.load(fd)
             print '%d Tags Identified' % len(obj['data'])
-            print json.dumps(object)
+            print json.dumps(obj)
 
         except:
             print 'There was an error.'
